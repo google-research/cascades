5c7939153fd23112a48ce3d89fe0eba8606e7899
Daniel Freeman <cdfreeman@google.com>
2023-01-09 09:52:30 -0800

    adding MATH dataloader
    PiperOrigin-RevId: 500738894

---
+++ cascades/examples/tasks/math.py
@@ -0,0 +1,42 @@
+# Copyright 2022 The cascades Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Loaders for MATH dataset."""
+import json
+
+open_file = open
+MATH_PATH = None
+
+
+def load_dataset(base_dir=MATH_PATH):
+  """Load MATH raw data from disk.
+
+  Available at:
+    https://github.com/hendrycks/math/
+
+  Args:
+    base_dir: Directory containing `train.jsonl` and `test.jsonl`.
+
+  Returns:
+    A dictionary of (train, test).
+    Each example is a dict with keys:
+    'index': (int) example number
+    'level': (int) difficulty
+    'problem': (str) problem statement
+    'solution': (str) an example solution
+  """
+  with open_file(base_dir, 'r') as f:
+    data = json.loads(f.readlines()[0])
+
+  return data
